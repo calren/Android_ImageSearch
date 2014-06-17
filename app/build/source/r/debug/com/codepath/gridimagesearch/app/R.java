d9b8eb193a368f2d5a0f1cde105cee1243380ce3
caren <cchang@tagged.com>
2014-06-17 00:47:33 -0700

    changing some styling

--- app/build/source/r/debug/com/codepath/gridimagesearch/app/R.java
+++ app/build/source/r/debug/com/codepath/gridimagesearch/app/R.java
@@ -102,9 +102,10 @@ containing a value of this type.
         public static final int search=0x7f020003;
     }
     public static final class id {
-        public static final int action_refresh=0x7f0a0004;
-        public static final int action_search=0x7f0a0005;
-        public static final int action_settings=0x7f0a0003;
+        public static final int action_refresh=0x7f0a0005;
+        public static final int action_search=0x7f0a0006;
+        public static final int action_settings=0x7f0a0004;
+        public static final int editText=0x7f0a0003;
         public static final int gvResults=0x7f0a0001;
         public static final int ivResult=0x7f0a0000;
         public static final int tvPlaceHolder=0x7f0a0002;
@@ -112,7 +113,8 @@ containing a value of this type.
     public static final class layout {
         public static final int activity_image_display=0x7f030000;
         public static final int activity_search=0x7f030001;
-        public static final int item_image_result=0x7f030002;
+        public static final int activity_settings=0x7f030002;
+        public static final int item_image_result=0x7f030003;
     }
     public static final class menu {
         public static final int image_display=0x7f090000;
@@ -130,10 +132,10 @@ containing a value of this type.
         public static final int type_prompt=0x7f070007;
     }
     public static final class style {
-        /**  Customize your theme here. 
-         */
         public static final int AppTheme=0x7f080000;
-        public static final int SettingsTheme=0x7f080001;
+        public static final int MyActionBar=0x7f080001;
+        public static final int MyTheme_ActionBar_TitleTextStyle=0x7f080002;
+        public static final int SettingsTheme=0x7f080003;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
